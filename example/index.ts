ca362f27d96edec1cdb27d381d0516f843186ae1
Erik Agsjö <erik.agsjo@gmail.com>
2020-10-22 09:42:38 +0200

    Example fix

--- example/index.ts
+++ example/index.ts
@@ -2,62 +2,65 @@ import AdminBro, { BaseRecord } from "admin-bro";
 import { FieldFilter, FindOptions, GraphQLAdapter, GraphQLConnection } from "../src";
 AdminBro.registerAdapter(GraphQLAdapter);
 
-const connection = new GraphQLConnection({ name: "Stuff", url: "http://localhost:3000/graphql" }, [
-    {
-        id: "things",
+const connection = new GraphQLConnection(
+    [
+        {
+            id: "things",
 
-        count: (filter: FieldFilter[]) => ({
-            query: `{
+            count: (filter: FieldFilter[]) => ({
+                query: `{
                 thingsCount(matching: $filter)
             }`,
-            variables: {
-                filter
-            },
-            parseResult(result: Record<string, number>) {
-                return result.thingsCount;
-            }
-        }),
+                variables: {
+                    filter
+                },
+                parseResult(result: Record<string, number>) {
+                    return result.thingsCount;
+                }
+            }),
 
-        find: (filter: FieldFilter[], options: FindOptions) => ({
-            query: `{
+            find: (filter: FieldFilter[], options: FindOptions) => ({
+                query: `{
                 things(matching: $filter, offset: $offset, limit: $limit) {
                     id
                     name
                     size
                 }
             }`,
-            variables: {
-                filter,
-                offset: options.offset,
-                limit: options.limit,
-            },
-            parseResult(result: Record<string, BaseRecord[]>): BaseRecord[] {
-                return result.things;
-            }
-        }),
+                variables: {
+                    filter,
+                    offset: options.offset,
+                    limit: options.limit,
+                },
+                parseResult(result: Record<string, BaseRecord[]>): BaseRecord[] {
+                    return result.things;
+                }
+            }),
 
-        findOne: (id: string | number) => ({
-            query: `{
+            findOne: (id: string | number) => ({
+                query: `{
                 thingById(id: $id){
                     id
                     name
                     size
                 }
             }`,
-            variables: {
-                id
-            },
-            parseResult(result: Record<string, BaseRecord | null>) {
-                return result.thingById;
-            }
-        })
-    }
-]);
+                variables: {
+                    id
+                },
+                parseResult(result: Record<string, BaseRecord | null>) {
+                    return result.thingById;
+                }
+            })
+        }
+    ],
+    { name: "Stuff", url: "http://localhost:3000/graphql" }
+);
 
 connection.init().then(() => {
     new AdminBro({
         // databases: [connection],
-        resources: [{resource: connection.r.things}],
+        resources: [{ resource: connection.r.things }],
         rootPath: "/admin",
     });
 }).catch((err: Error) => {
